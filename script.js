215b82af3459a1487caf1b296484008faeb1fbe9
ayaz <vefaayazwx@gmail.com>
2022-12-06 11:00:45 +0300

    dark mode inserted

--- script.js
+++ script.js
@@ -57,3 +57,31 @@ options.forEach(operator => {
         opt = newOpt;
     }
 })
+
+
+var ball = document.querySelector("#ball");
+
+ball.addEventListener("click", changeTheme);
+
+var leftside = true;
+function changeTheme(){
+    if (leftside == true) {
+        ball.style.left = "35px";
+        ball.style.backgroundColor = "#111";
+        leftside = false;
+        document.querySelector(".mode").style.backgroundColor = "#fff";
+        document.querySelector("body").style.backgroundColor = ("#111");
+        document.querySelector("#calculator").style.backgroundColor = "grey";
+        document.querySelector("table").style.backgroundColor = ("grey");
+        document.querySelector.All("td").style.backgroundColor = ("white");
+    } else {
+        ball.style.left = "5px";
+        ball.style.backgroundColor = "#fff";
+        leftside = true;
+        document.querySelector(".mode").style.backgroundColor = "#111";
+        document.querySelector("body").style.backgroundColor = ("#ccc");
+        document.querySelector("#calculator").style.backgroundColor = "white";
+        document.querySelector("table").style.backgroundColor = ("white");
+        document.querySelector.All("td").style.color = ("black");
+    }
+}
